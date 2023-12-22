850c4fbf1f65989cba0e0a64eca9b13cda126e45
razz3232 <74662119+razz3232@users.noreply.github.com>
2023-12-22 17:00:41 +0000

    changed file name

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -2,7 +2,7 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-class Main {
+class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Kamusta Mundo!"); // Display the string.
     }
